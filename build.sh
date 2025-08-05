43e5c7668a9fa5e4fd6a042ecf25dca2945dcfc8
esmond <dhonniemccony3@gmail.com>
2025-08-04 21:01:07 -1200

    updated build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 
 # pip install -r requirements.txt
 
-python Diploma-Farmer-Connect/manage.py collectstatic --no-input
+python dipbackend/manage.py collectstatic --no-input
 
-python Diploma-Farmer-Connect/manage.py migrate 
+python dipbackend/manage.py migrate 
 
-python Diploma-Farmer-Connect/manage.py runserver 0.0.0.0:$PORT
+python dipbackend/manage.py runserver 0.0.0.0:$PORT
