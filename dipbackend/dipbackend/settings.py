eb1a7de314a924964a8c1a877bc16898f5388afa
esmond <dhonniemccony3@gmail.com>
2025-08-07 01:13:47 -1200

    updated settings.py

--- dipbackend/dipbackend/settings.py
+++ dipbackend/dipbackend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 # from decouple import config
-import cloudinary,cloudinary_storage # type: ignore
+# import cloudinary,cloudinary_storage # type: ignore
 
 from dotenv import load_dotenv
 
